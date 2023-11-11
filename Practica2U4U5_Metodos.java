4870c2d785c7744ed60066452d9a6282aa381974
Anton1oC4astr0 <143725409+Anton1oC4astr0@users.noreply.github.com>
2023-11-10 21:46:29 -0600

    Update Practica2U4U5_Metodos.java

--- Practica2U4U5_Metodos.java
+++ Practica2U4U5_Metodos.java
@@ -4,19 +4,19 @@ public class Practica2U4U5_Metodos {
 
     public static void Suma(int x, int y) {
         int suma = x+y;
-        System.out.println("El resultado al sumar los numeros ingresados es de: " + suma);
+        System.out.println("El resultado al sumar los numeros establecidos es de: " + suma);
     }
     public static void Multiplicacion(int x, int y){
         int multiplicacion = x*y;
-        System.out.println("El resultado al multiplicar los numeros ingresados es de: " + multiplicacion);
+        System.out.println("El resultado al multiplicar los numeros establecidos es de: " + multiplicacion);
     }
     public static void Resta(int x, int y){
         int resta = x-y;
-        System.out.println("El resultado al restar los numeros ingresados es de: " + resta);
+        System.out.println("El resultado al restar los numeros establecidos es de: " + resta);
     }
     public static void Division(int x, int y){
         int division = x/y;
-        System.out.println("El resultado al dividir los numeros ingresados es de: " + division);
+        System.out.println("El resultado al dividir los numeros establecidos es de: " + division);
     }
     public static void main (String []args){
         int x = 20;
